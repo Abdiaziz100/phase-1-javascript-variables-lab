e86301de2c4526afb4868ab4810aea79e58ab0dc
mohamed <abdiazizmahat@gmail.com>
2025-06-25 18:45:53 +0300

    phase-1-javascript-variables-lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+const companyName = 'Scuber'; // ✅ Must use const
+
+let mostProfitableNeighborhood = 'Chelsea'; // ✅ let is fine here
+
+let companyCeo = 'Susan Smith'; // ✅ or const is fine if it won't be changed
+
